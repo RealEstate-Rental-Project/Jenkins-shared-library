d539900f304e5a6c036ff2c9ee0410c163b9e63d
Yassine Kamouss <156694023+yassinekamouss@users.noreply.github.com>
2026-01-12 03:06:00 +0100

    Enhance pipeline with SonarQube project key and latest tag

--- vars/standardBackendPipeline.groovy
+++ vars/standardBackendPipeline.groovy
@@ -1,16 +1,16 @@
-// vars/standardBackendPipeline.groovy
 def call(Map config = [:]) {
     def dockerRegistry = config.registry ?: "yassinekamouss"
     def appName = config.appName
-    
+    def sonarProjectKey = config.sonarProjectKey ?: appName // si non fourni, on utilise appName
+
     pipeline {
         agent any 
-        
+
         tools {
             maven 'maven-3' 
             jdk 'jdk-17'
         }
-        
+
         stages {
             stage('Checkout & Init') {
                 steps {
@@ -18,10 +18,8 @@ def call(Map config = [:]) {
                     script {
                         env.IMAGE_TAG = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
                         env.FULL_IMAGE_NAME = "${dockerRegistry}/${appName}:${env.IMAGE_TAG}"
-                        
-                        // 1. AJOUT : Définir la variable pour le tag latest
                         env.LATEST_IMAGE_NAME = "${dockerRegistry}/${appName}:latest"
-                        
+
                         echo "Version : ${env.IMAGE_TAG}"
                         echo "Image tagguée : ${env.FULL_IMAGE_NAME}"
                         echo "Image latest : ${env.LATEST_IMAGE_NAME}"
@@ -37,12 +35,9 @@ def call(Map config = [:]) {
 
             stage('SonarQube Analysis') {
                 steps {
-                    withSonarQubeEnv('SonarQube') {
-                        sh """
-                          mvn sonar:sonar \
-                            -Dsonar.projectKey=${appName} \
-                            -Dsonar.projectName=${appName}
-                        """
+                    script {
+                        // Appel de la shared library Sonar avec le projectKey spécifique
+                        runSonarAnalysis(sonarProjectKey, 'maven')
                     }
                 }
             }
@@ -57,11 +52,9 @@ def call(Map config = [:]) {
                 }
             }
 
-
             stage('Docker Build') {
                 steps {
                     script {
-                        // 2. MODIFICATION : Ajouter un deuxième flag -t pour le tag latest
                         sh "docker build -t ${env.FULL_IMAGE_NAME} -t ${env.LATEST_IMAGE_NAME} ."
                     }
                 }
@@ -72,8 +65,6 @@ def call(Map config = [:]) {
                     script {
                         withCredentials([usernamePassword(credentialsId: 'DOCKER_HUB', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
                             sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
-                            
-                            // 3. MODIFICATION : Pousser les deux tags
                             sh "docker push ${env.FULL_IMAGE_NAME}"
                             sh "docker push ${env.LATEST_IMAGE_NAME}"
                         }
@@ -81,7 +72,7 @@ def call(Map config = [:]) {
                 }
             }
         }
-        
+
         post {
             always {
                 cleanWs()
